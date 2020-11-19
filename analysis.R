536515b495e2225817abfab72d3c8aac8af77438
yoonseosong <ysong0915@gmail.com>
2020-11-19 18:44:16 +0900

    complete analysis

--- analysis.R
+++ analysis.R
@@ -16,20 +16,42 @@ timestamps_by_state <- raw_data %>%
   count()
 
 # Formatting: split out state name from electoral votes
+# Add biden and trump vote columns
 data <- raw_data %>% 
-  separate(state, into = c("state_name", "ev"), " \\(") %>% 
-  mutate(ev = parse_number(ev))
-  View()
+  separate(state, into = c("state", "ev"), " \\(") %>% 
+  mutate(ev = parse_number(ev)) %>% 
+  mutate(biden_votes = 
+           if_else(leading_candidate_name == "Biden", 
+                   leading_candidate_votes, 
+                   trailing_candidate_votes),
+         trump_votes = total_votes_count - biden_votes)
 
 # How many reported timestamps exist for each state?
 
 # When did Biden take the lead in Georgia?
 ga_lead_time <- data %>% 
-  filter(state == "Georgia (EV: 16)", leading_candidate_name == "Biden") %>% 
+  filter(state == "Georgia", leading_candidate_name == "Biden") %>% 
   filter(timestamp == min(timestamp)) %>% 
   pull(timestamp)
 
 # What is the difference in votes in each state?
+# (at the most recent timestamp)
+vote_diff <- data %>% 
+  group_by(state) %>% 
+  filter(timestamp == max(timestamp)) %>% 
+  mutate(vote_diff = biden_votes - trump_votes, 
+         pct_diff = vote_diff / total_votes_count)
+
+vote_diff_plot <- ggplot(vote_diff) +
+  geom_col(mapping = aes(x = vote_diff, 
+                         y = reorder(state, vote_diff),
+                         fill = leading_candidate_name)) +
+  scale_fill_manual(values = c("blue", "red")) +
+  labs(y = "State", x = "Vote Difference", 
+       fill = "Candidate", title = "Vote difference at most recent time stamp")
+
+vote_pct_plot <- ggplot(vote_diff) +
+  geom_col(mapping = aes(x = pct_diff, y = reorder(state, pct_diff)))
 
 # How do total votes change over time (by candidate)?
 
